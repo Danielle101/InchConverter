d86bf20d5e3b78dd32913a2ed902b09c0dd8fb12
dh <halldan4@yahoo.com>
2016-05-16 22:28:20 -0500

    added formula for millimeter & yards

--- src/inchConverter/InchConverter.java
+++ src/inchConverter/InchConverter.java
@@ -9,9 +9,18 @@ System.out.println("Welcometo the inch conversion amount!");
 System.out.println("Please enter your measurement in inches:");
 double userInch=sc.nextDouble();
 if (userInch > 0 ){
+	double MilliCon= userInch *25.4;
+	double CentiCon= userInch *2.54;
 	double feetCon= userInch * 0.0833333;
+	double yardCon= userInch *0.0277778;
 	System.out.println("\nConversion of "+userInch+" inches:\n==========================");
 	System.out.println(feetCon + " feet");
+	System.out.println(CentiCon + " centimeters");
+	System.out.println(MilliCon + " millimeters");
+	System.out.println(yardCon + " yards");
+
+
+	
 }
 	}
 
