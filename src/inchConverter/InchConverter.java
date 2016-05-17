3e9eea6d7c1417a7a528bffea2c568fa895117b2
dh <halldan4@yahoo.com>
2016-05-16 22:20:12 -0500

    added 1st conversion formula

--- src/inchConverter/InchConverter.java
+++ src/inchConverter/InchConverter.java
@@ -4,7 +4,15 @@ public class InchConverter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner (System.in);
+System.out.println("Welcometo the inch conversion amount!");
+System.out.println("Please enter your measurement in inches:");
+double userInch=sc.nextDouble();
+if (userInch > 0 ){
+	double feetCon= userInch * 0.0833333;
+	System.out.println("\nConversion of "+userInch+" inches:\n==========================");
+	System.out.println(feetCon + " feet");
+}
 	}
 
 }
