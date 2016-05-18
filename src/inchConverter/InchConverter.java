5b02b6cbbb1dab6e32373478007b148ae0048138
dh <halldan4@yahoo.com>
2016-05-17 21:02:27 -0500

    updated welcome message and user prompts

--- src/inchConverter/InchConverter.java
+++ src/inchConverter/InchConverter.java
@@ -1,27 +1,33 @@
+/*Date: 5/16/16
+Description: Converts inches to other units of measurement*/
 package inchConverter;
+
 import java.util.*;
+
 public class InchConverter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner (System.in);
-System.out.println("Welcometo the inch conversion amount!");
-System.out.println("Please enter your measurement in inches:");
-double userInch=sc.nextDouble();
-if (userInch > 0 ){
-	double MilliCon= userInch *25.4;
-	double CentiCon= userInch *2.54;
-	double feetCon= userInch * 0.0833333;
-	double yardCon= userInch *0.0277778;
-	System.out.println("\nConversion of "+userInch+" inches:\n==========================");
-	System.out.println(feetCon + " feet");
-	System.out.println(CentiCon + " centimeters");
-	System.out.println(MilliCon + " millimeters");
-	System.out.println(yardCon + " yards");
+		// created scanner for user input
+		Scanner sc = new Scanner(System.in);
+		// welcome message
+		System.out.println("Welcometo the inch conversion amount!");
+		// ask for user input
+		System.out.println("Please enter your measurement in inches:");
+		double userInch = sc.nextDouble();
+		if (userInch > 0) {
+			double MilliCon = userInch * 25.4;
+			double CentiCon = userInch * 2.54;
+			double feetCon = userInch * 0.0833333;
+			double yardCon = userInch * 0.0277778;
+			System.out.println("\nConversion of " + userInch + " inches:\n==========================");
+			System.out.println(feetCon + " feet");
+			System.out.println(CentiCon + " centimeters");
+			System.out.println(MilliCon + " millimeters");
+			System.out.println(yardCon + " yards");
 
-
-	
-}
+		}
+		sc.close();
 	}
 
 }
