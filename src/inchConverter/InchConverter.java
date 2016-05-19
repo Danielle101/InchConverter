7828203888bb560b4abfdc08895e8ae06061e7f8
dh <halldan4@yahoo.com>
2016-05-18 21:52:03 -0500

    added while loop for user entering multiple numbers and added an exit message

--- src/inchConverter/InchConverter.java
+++ src/inchConverter/InchConverter.java
@@ -10,31 +10,35 @@ public class InchConverter {
 		// TODO Auto-generated method stub
 		// created scanner for user input
 		Scanner sc = new Scanner(System.in);
+		String choice = "y";
 		// welcome message
 		System.out.println("Welcome to the inch conversion app!");
 		// ask for user input
-		System.out.println("Please enter your measurement in inches:");
-		double userInch = sc.nextDouble();
-		System.out.println("What unit of measurement would you like to convert inches to?\n");
-		System.out.println("Selection Menu:\n1 for Millimeters\n2 for Centimeters\n3 for Feet\n4 for Yards");
-		System.out.println("\nPlease enter a number (1-4):");
-		int userSelect = sc.nextInt();
-		if (userSelect == 1){
-			double MilliCon = userInch * 25.4;
-			System.out.println(userInch+" inches is equivalent to "+MilliCon + " millimeters.");
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.println("Please enter your measurement in inches:");
+			double userInch = sc.nextDouble();
+			System.out.println("What unit of measurement would you like to convert inches to?\n");
+			System.out.println("Selection Menu:\n1 for Millimeters\n2 for Centimeters\n3 for Feet\n4 for Yards");
+			System.out.println("\nPlease enter a number (1-4):");
+			int userSelect = sc.nextInt();
+			if (userSelect == 1) {
+				double MilliCon = userInch * 25.4;
+				System.out.println(userInch + " inches is equivalent to " + MilliCon + " millimeters.");
+			} else if (userSelect == 2) {
+				double CentiCon = userInch * 2.54;
+				System.out.println(userInch + " inches is equivalent to " + CentiCon + " centimeters.");
+			} else if (userSelect == 3) {
+				double feetCon = userInch * 0.0833333;
+				System.out.println(userInch + " inches is equivalent to " + feetCon + " feet.");
+			} else if (userSelect == 4) {
+				double yardCon = userInch * 0.0277778;
+				System.out.println(userInch + " inches is equivalent to " + yardCon + " yards.");
 			}
-		else if (userSelect == 2){
-			double CentiCon = userInch * 2.54;
-			System.out.println(userInch+" inches is equivalent to "+CentiCon + " centimeters.");
+				System.out.println("\nWould you like to convert another number? (y/n):");
+				choice = sc.next();
 			}
-		else if (userSelect == 3){
-			double feetCon = userInch * 0.0833333;
-			System.out.println(userInch+" inches is equivalent to "+feetCon + " feet.");
-			}
-		else if (userSelect == 4){double yardCon = userInch * 0.0277778;			
-			System.out.println(userInch+" inches is equivalent to "+yardCon + " yards.");
-		}
+		System.out.println("Thanks for using the Inch Converter App! Come back soon!");
+		sc.close();
 		}
 	}
-
 
